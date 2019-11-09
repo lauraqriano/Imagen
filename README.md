8325f7670a5a82174a10c9a1e4415e21783af49c
lauraqriano <lauraquintero9218@gmail.com>
2019-11-09 11:38:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-kfnzgc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-kfnzgc)
---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+  // Vamos a crear una serie de variables de diferentes tipos
+  // Numérico
+  iva = 16;        // Variable tipo entero
+  total = 234.65;  // Variable tipo decimal
+
+  // Cadenas de texto, tambien se les dicen String
+  mensaje = 'Bienvenid@ a She Codes Angular';
+  nombreCompleto = 'Alejandra Giraldo';
+
+  // Arrays - Arreglos
+  dias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+  diaSeleccionado = this.dias[0];    // diaSeleccionado = 'Lunes'
+
+  // Object - Objetos
+  personalInfo = {'nombre': 'Alejandra', 'apellido': 'Giraldo', 'cedula': 123456};
+  soloApellido = this.personalInfo.apellido;    // soloApellido = 'Giraldo'
+
+  // Boolean - Booleanos
+  estoyAprendiendo = true;
+  meGustanLosVegetales = false;
+}
